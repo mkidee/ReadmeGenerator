5acacda73d9294c125e3bda7123ce784d53e6461
mkidee <mckaymemmott@gmail.com>
2023-04-23 17:36:35 -0600

    created questions for array, addec comments to each

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // Create the project title (make sure is bigger font than the rest) 
+    "What is your project title?",
+    // add to the Desctiption section
+    "What is the description of your project?",
+    // add to the Installation section
+    "How does a user install your project?",
+    // add to the Usage section
+    "How should this project be used?",
+    //this will be added to the Contributing section
+    "Were there any other Collaboraters on this project? (If yes, please include their name.)",
+    // will be added to the Test section of the readme
+    "What are the instrucitons for a user to test this project?",
+    // license must be chosen from a dropdown, add badge to the top, and license info in the License section
+    "If any, what licenses are included in this project?",
+    // the next two questions will be added to the Questions section:
+    "What is your GitHub username?", // username must be linked to the actual profile
+    "What is your email address?"
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
