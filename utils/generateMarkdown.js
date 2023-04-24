72b800cbb9ef5f50171d58697dacdc555c5a4be2
mkidee <mckaymemmott@gmail.com>
2023-04-24 12:39:03 -0600

    renderlicensesection

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,6 +4,9 @@ function renderLicenseBadge(license) {
   let badgeL = '' 
 
   switch(license){
+    default:
+      badge = '';
+
     case 'Apache 2.0':
       badgeL = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]'
       break;
@@ -24,15 +27,55 @@ function renderLicenseBadge(license) {
       badgeL = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
       break;
   }
+
+  return badgeL;
+
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let link = ''
+  switch(license) {
+    default:
+      link = '';
+
+      case "Apache 2.0":
+      link = "https://opensource.org/licenses/Apache-2.0";
+      break;
+
+    case "BSD 3-Clause":
+      link = "https://opensource.org/licenses/BSD-3-Clause";
+      break;
+
+    case "GPL 3.0":
+      link = "https://www.gnu.org/licenses/gpl-3.0";
+      break;
+
+    case "ISC":
+      link = "https://opensource.org/licenses/ISC";
+      break;
+
+    case "MIT":
+      link = "https://opensource.org/licenses/MIT";
+      break;
+  }
+  return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licSection = ''
+
+  if (license = 'N/A') {
+    licSection = '## License'
+    licSection += 'This program/application uses the ' + '[' + license + ']('+ renderLicenseLink(license) +') license.';
+  }
+
+  return licSection;
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
