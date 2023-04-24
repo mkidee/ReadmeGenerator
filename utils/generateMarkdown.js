3be83c2c4925b1e9cdf562387b8fecb70d05215d
mkidee <mckaymemmott@gmail.com>
2023-04-24 11:39:04 -0600

    added in the badges for the list selection

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badgeL = '' 
+
+  switch(license){
+    case 'Apache 2.0':
+      badgeL = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]'
+      break;
+
+    case 'BSD 3-Clause':
+      badgeL = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]'
+      break;
+
+    case 'GPL 3.0':
+      badgeL = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]'
+      break;
+
+    case 'ISC':
+      badgeL = '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]'
+      break;
+
+    case 'MIT':
+      badgeL = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
