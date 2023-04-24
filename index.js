44b3eb90e9a334b1557dec937466cf29e41fe2c5
mkidee <mckaymemmott@gmail.com>
2023-04-23 21:09:46 -0600

    created questions with their respective inputs, functions at bottom of file

--- index.js
+++ index.js
@@ -1,31 +1,80 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+const licenses = ["Apache 2.0", "BSD 3-Clause", "GPL 3.0", "ISC", "MIT", "N/A"];
 
 // TODO: Create an array of questions for user input
 const questions = [
-    // Create the project title (make sure is bigger font than the rest) 
-    "What is your project title?",
-    // add to the Desctiption section
-    "What is the description of your project?",
-    // add to the Installation section
-    "How does a user install your project?",
-    // add to the Usage section
-    "How should this project be used?",
-    //this will be added to the Contributing section
-    "Were there any other Collaboraters on this project? (If yes, please include their name.)",
-    // will be added to the Test section of the readme
-    "What are the instrucitons for a user to test this project?",
-    // license must be chosen from a dropdown, add badge to the top, and license info in the License section
-    "If any, what licenses are included in this project?",
-    // the next two questions will be added to the Questions section:
-    "What is your GitHub username?", // username must be linked to the actual profile
-    "What is your email address?"
+    {
+    // Create the project title (make sure is bigger font than the rest)
+        type: "input",
+        name: "title",
+        message: "What is your project title?",
+        validate: validateRequiredUserInput
+},
+    {
+        type: "input",
+        name: "description",
+        message: "What is the description of your project?",
+        validate: validateRequiredUserInput
+},
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps for a user to install your project?",
+},
+    {
+        type: "input",
+        name: "usage",
+        message: "How is this project used?",
+        validate: validateRequiredUserInput
+},
+    {
+        type: "input",
+        name: "credit",
+        message: "If there are any collaborators or third-party services being used, please mention them here:",
+},
+    {
+        type: "list",
+        name: "licenses",
+        message: "Please select the license applicable to your project:",
+        choices: licenses,
+        validate: validateRequiredUserInput
+},
+    {
+        type: "input",
+        name: "test",
+        message: "What tests (if any) were used for this project?",
+        validate: validateRequiredUserInput
+},
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+        validate: validateRequiredUserInput
+},
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email? (For contact and questions)",
+        validate: validateRequiredUserInput
+}
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    const genFile = "./readmeGen/README.md"
+
+    fs.writeFile(genFile, generateMarkdown(data), function(err){
+        err ? console.log(err) : console.log("Awesome! " + genFile + " created.")
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(data => writeToFile(data))
+}
 
 // Function call to initialize app
 init();
